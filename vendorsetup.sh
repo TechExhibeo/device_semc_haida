b9a9d66ffb562267b09f6c4739292bd02ed4211c
Deepak Sharma <TechExhibeo@gmail.com>
2015-05-02 20:45:44 +0530

    validus-ify

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -12,4 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-add_lunch_combo cm_haida-eng
+add_lunch_combo validus_haida-userdebug
+add_lunch_combo validus_haida-eng
